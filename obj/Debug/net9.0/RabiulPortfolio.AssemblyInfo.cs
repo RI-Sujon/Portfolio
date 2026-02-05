dd74e4a44eef6e1bb5d88d8611c6b0cf98192692
RabiulBJIT <rabiul.islam@bjitgroup.com>
2026-02-05 14:16:14 +0600

    updated ui

--- obj/Debug/net9.0/RabiulPortfolio.AssemblyInfo.cs
+++ obj/Debug/net9.0/RabiulPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RabiulPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2fcd6bf38efe01628ffa262b2d80df558a7516")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26b235a868125e34a6411bef2f1e7c39ab15c44c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RabiulPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RabiulPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
